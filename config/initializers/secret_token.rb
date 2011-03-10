36488df8b5ce087baa500daed1cfced5ad181f9c
rposborne <russell@burningpony.com>
2011-03-10 16:13:00 -0500

    Initial Commit
    Signed-off-by: rposborne <russell@burningpony.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhdChecker::Application.config.secret_token = 'af97ec26fda1b33535d16401b1fef5d4f856933e5e30f9a43ba1a3857b0a3e1055190956cdedb2bdbd8bb0ed12b0c4f8e9c27dd80a92e1d8178029e434f3335d'
