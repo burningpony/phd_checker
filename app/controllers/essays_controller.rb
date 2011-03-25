4a344bbf7f476c2f6a20f592cbf5f890d30495c3
Ben Congleton <ben@hab.la>
2011-03-25 15:47:04 -0400

    untested

---
+++ app/controllers/essays_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  
+  def show
+    render :action => params[:id].match(/\d+/)[0]
+  end
+
+  def list
+    @essays = []
+    view_directory = File.expand_path('../views/essays', __FILE__)
+    Dir.foreach(view_directory).each do |view|
+      if view.match(/\d/)
+        @essays << view
+      end
+    end
+  end
+  
+end
