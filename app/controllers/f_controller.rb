c5d69cef0a64b217f4d5b792d35910ec8cd342f5
rposborne <russell@burningpony.com>
2011-03-30 22:54:07 -0400

    Scorecard f works
    Requires passing Participant ID as user_id

--- app/controllers/f_controller.rb
+++ app/controllers/f_controller.rb
@@ -1,8 +1,9 @@
 class FController <EssayBaseController
   def score_card
-    @user = User.find(params[:participant_id])
+    @user = User.find(params[:user_id])
     @responses = @user.responses
     
     @payment = 30
+    render :file => "essays/score_card"
   end
 end
