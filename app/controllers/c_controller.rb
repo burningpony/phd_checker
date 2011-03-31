37824a881418c9fb435f1592c3ce4d151ca5d93a
rposborne <russell@burningpony.com>
2011-03-30 23:08:16 -0400

    3 out of 6 scored cards done

--- app/controllers/c_controller.rb
+++ app/controllers/c_controller.rb
@@ -2,8 +2,9 @@ class CController <  EssayBaseController
   def score_card
     @user = User.find(params[:participant_id])
     @responses = @user.responses
+    #	Fee For Service
+    @payment = @responses.count * 0.25
     
-    @payment = 30
     render :file => "essays/score_card"
   end
 end
--- app/controllers/d_controller.rb
+++ app/controllers/d_controller.rb
@@ -2,8 +2,14 @@ class DController <  EssayBaseController
   def score_card
     @user = User.find(params[:participant_id])
     @responses = @user.responses
+   # 4.	Fee For Service plus Quota Bonus
+    @payment =  @responses.count * 0.25
     
-    @payment = 30
+    
+    #set the quota here 
+    @responses.count > 1 ? @payment += 1 : @payment
+    
+    
     render :file => "essays/score_card"
   end
 end
