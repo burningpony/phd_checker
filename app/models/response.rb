3488413b4cc43746fa8a5f688ca2c99edc6cfcc3
rposborne <russell@burningpony.com>
2011-03-30 22:32:47 -0400

    Updated Views TO better Reflect Data

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user_id
+  scope :incorrect, where(:correct => false)
+  scope :correct, where(:correct => true)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(:version => 20110331013107) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "quota"
-    t.string   "group"
   end
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "group"
   end
 
 end
