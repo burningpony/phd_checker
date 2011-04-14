a651fbaa58233389c05f8b0a065a325203cb75d5
econglet <econglet@vt.edu>
2011-04-14 13:28:22 -0700

    Edited app/controllers/b_controller.rb via GitHub

--- app/controllers/b_controller.rb
+++ app/controllers/b_controller.rb
@@ -3,10 +3,10 @@ class BController <  EssayBaseController
     @user = User.find_or_create_by_id(params[:participant_id])
     @responses = @user.responses
     completed_essay = @user.responses.collect {|x| x.essay }.uniq.count
-    @payment = 3*completed_essay
+    @payment = 2.5*completed_essay
     #set the quota here 
     
-    @responses.count > 1 ? @payment += 1 : @payment
+    @responses.count > 9 ? @payment += 1 : @payment
     render :file => "essays/score_card.html.haml", :layout => false
   end
   
