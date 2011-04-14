0ee8cf44fabb3813fdd987504f8c10a87b2a5815
econglet <econglet@vt.edu>
2011-04-14 13:27:59 -0700

    Edited app/controllers/a_controller.rb via GitHub

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -3,7 +3,7 @@ class AController <  EssayBaseController
     @user = User.find_or_create_by_id(params[:participant_id])
     @responses = @user.responses
     completed_essay = @user.responses.collect {|x| x.essay }.uniq.count
-    @payment = 3*completed_essay
+    @payment = 2.5*completed_essay
     render :file => "essays/score_card.html.haml", :layout => false
   end
 end
