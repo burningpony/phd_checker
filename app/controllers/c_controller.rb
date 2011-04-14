e3b4996d16d2959b61db347332907db3600ba678
econglet <econglet@vt.edu>
2011-04-14 13:28:56 -0700

    Edited app/controllers/c_controller.rb via GitHub

--- app/controllers/c_controller.rb
+++ app/controllers/c_controller.rb
@@ -3,7 +3,7 @@ class CController <  EssayBaseController
     @user = User.find_or_create_by_id(params[:participant_id])
     @responses = @user.responses
     #	Fee For Service
-    @payment = @responses.count * 0.25
+    @payment = @responses.count * 0.20
     
     render :file => "essays/score_card.html.haml", :layout => false
   end
