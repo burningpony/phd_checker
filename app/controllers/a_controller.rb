53428b1e2ff029a2352908746e965bd71bb87f25
Sam Congleton <stcongleton@gmail.com>
2014-03-18 17:45:21 -0400

    Rounds now properly displays cumulative rounds as well as the previous round

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -1,8 +1,8 @@
 class AController <  EssayBaseController
   def score_card
     instance_variable_setup
-    round_completed_essay = @total_responses.collect {|x| x.essay }.uniq.count
-    total_completed_essay = @responses_from_round.collect {|x| [x.essay, x.round_number] }.uniq.count
+    round_completed_essay = @responses_from_round.collect {|x| x.essay }.uniq.count
+    total_completed_essay = @total_responses.collect {|x| [x.essay, x.round_number] }.uniq.count
     @total_payment = 2.5*total_completed_essay
     @round_payment = 2.5*round_completed_essay
     render :file => "essays/score_card.html.haml", :layout => false
--- app/controllers/b_controller.rb
+++ app/controllers/b_controller.rb
@@ -1,11 +1,11 @@
 class BController <  EssayBaseController
   def score_card
     instance_variable_setup
-    completed_essay = @total_responses.collect {|x| x.essay }.uniq.count
+    round_completed_essay = @responses_from_round.collect {|x| x.essay }.uniq.count
+    total_completed_essay = @total_responses.collect {|x| [x.essay, x.round_number] }.uniq.count
     
-    prev_completed_essay = @responses_from_round.collect {|x| [x.essay, x.round_number] }.uniq.count
-    @total_payment = 2.5*completed_essay
-    @round_payment = 2.5*prev_completed_essay
+    @total_payment = 2.5*total_completed_essay
+    @round_payment = 2.5*round_completed_essay
     #set the quota here 
     
     @responses_from_round.count > 9 ? @round_payment += 1 : @round_payment
