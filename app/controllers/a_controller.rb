afbc666906357f103d25b1410601dab77154f08f
Sam Congleton <stcongleton@gmail.com>
2014-03-18 17:01:25 -0400

    Fixed the prev_total payments for a and b controllers.

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -1,10 +1,10 @@
 class AController <  EssayBaseController
   def score_card
     instance_variable_setup
-    completed_essay = @user.total_responses.collect {|x| x.essay }.uniq.count
-    pre_completed_essay = @user.responses_from_round.collect {|x| x.essay }.uniq.count
-    @total_payment = 2.5*completed_essay
-    @round_payment = 2.5*prev_completed_essay
+    round_completed_essay = @total_responses.collect {|x| x.essay }.uniq.count
+    total_completed_essay = @responses_from_round.collect {|x| [x.essay x.round_number] }.uniq.count
+    @total_payment = 2.5*total_completed_essay
+    @round_payment = 2.5*round_completed_essay
     render :file => "essays/score_card.html.haml", :layout => false
   end
 end
--- app/controllers/b_controller.rb
+++ app/controllers/b_controller.rb
@@ -1,9 +1,9 @@
 class BController <  EssayBaseController
   def score_card
     instance_variable_setup
-    completed_essay = @user.total_responses.collect {|x| x.essay }.uniq.count
+    completed_essay = @total_responses.collect {|x| x.essay }.uniq.count
     
-    prev_completed_essay = @user.responses_from_round.collect {|x| x.essay }.uniq.count
+    prev_completed_essay = @responses_from_round.collect {|x| [x.essay x.round_number] }.uniq.count
     @total_payment = 2.5*completed_essay
     @round_payment = 2.5*prev_completed_essay
     #set the quota here 
