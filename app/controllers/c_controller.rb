3319d54e9042f4e4cc0bb5f77dee99f9bb818807
Sam Congleton <stcongleton@gmail.com>
2014-05-08 16:25:14 -0400

    Changed payment weight per response

--- app/controllers/c_controller.rb
+++ app/controllers/c_controller.rb
@@ -1,8 +1,8 @@
 class CController <  EssayBaseController
   def score_card
     instance_variable_setup
-    @total_payment = @total_responses.count * 0.20
-    @round_payment = @responses_from_round.count * 0.20
+    @total_payment = @total_responses.count * 0.45
+    @round_payment = @responses_from_round.count * 0.45
 
     render :file => "essays/score_card.html.haml", :layout => false
   end
