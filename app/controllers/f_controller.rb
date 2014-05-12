24503f22801415ac893a36aff2aa239967f3945d
Sam Congleton <stcongleton@gmail.com>
2014-05-12 19:47:37 -0400

    various changes to styling

--- app/controllers/f_controller.rb
+++ app/controllers/f_controller.rb
@@ -2,8 +2,8 @@ class FController <EssayBaseController
   def score_card
     instance_variable_setup
     @round_payment = 25
-    @total_payment = @total_responses.select(:round_number).distinct.count * 25
-    render :file => "essays/score_card.html.haml", :layout => false
+    @total_payment = (Integer(params[:round_number])-1) * 25
+    render :file => "essays/score_card.html.haml", :layout => "default"
   end
   
 
