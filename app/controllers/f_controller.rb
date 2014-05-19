189fd01ed3ee62fa69e165a7770986c17283d81d
Russell Osborne <russosborn@gmail.com>
2014-05-19 14:10:48 -0400

    Fixing a bug that would cause the process to restart for f treatment.

--- app/controllers/f_controller.rb
+++ app/controllers/f_controller.rb
@@ -2,6 +2,6 @@ class FController < EssayBaseController
   def score_card
     @round_payment = 25
     @total_payment = Integer(params[:round_number]) * 25
-    render file: 'essays/score_card.html.haml', layout: 'default'
+    render file: 'essays/score_card.html.haml', layout: false
   end
 end
