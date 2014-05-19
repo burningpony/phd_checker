0025dae8fa2882d1993f350ba4f0e328157b7550
Russell Osborne <russosborn@gmail.com>
2014-05-19 12:21:00 -0400

    More durable int casting.

--- app/controllers/essay_base_controller.rb
+++ app/controllers/essay_base_controller.rb
@@ -38,7 +38,7 @@ class EssayBaseController < ApplicationController
   def instance_variable_setup
     @user = User.find_or_create_by_id(params[:participant_id])
     @total_responses = @user.responses
-    @round = Integer(params[:round_number]) || 1
+    @round = (params[:round_number] || 1).to_i
     previous_round = @round - 1
     @responses_from_round = @user.responses.where(round_number: previous_round)
   end
