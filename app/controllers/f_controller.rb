0817fbf0629c464d824ced4d7a45e14ac1c6b0c6
Russell Osborne <russosborn@gmail.com>
2014-05-21 10:42:51 -0400

    Force cast typing for @round.

--- app/controllers/f_controller.rb
+++ app/controllers/f_controller.rb
@@ -1,7 +1,7 @@
 class FController < EssayBaseController
   def score_card
     @round_payment = 25
-    @total_payment = @round * 25
+    @total_payment = @round.to_i * 25
     render file: 'essays/score_card.html.haml', layout: false
   end
 end
