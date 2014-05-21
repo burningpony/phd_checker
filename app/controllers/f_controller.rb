e3884adf56ac32c2ec8b07a6348f003f0e1fefcd
Russell Osborne <russosborn@gmail.com>
2014-05-21 10:23:59 -0400

    Use the pre calculated round.

--- app/controllers/f_controller.rb
+++ app/controllers/f_controller.rb
@@ -1,7 +1,7 @@
 class FController < EssayBaseController
   def score_card
     @round_payment = 25
-    @total_payment = Integer(params[:round_number]) * 25
+    @total_payment = @round * 25
     render file: 'essays/score_card.html.haml', layout: false
   end
 end
