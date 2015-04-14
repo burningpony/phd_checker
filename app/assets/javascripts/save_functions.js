ebab2e8d7f0afe4bfa7a085c5771bf427768223d
scong <sam@burningpony.com>
2015-04-14 19:06:25 -0400

    Move some more ajax calls into save_functions.js

---
+++ app/assets/javascripts/save_functions.js
@@ -0,0 +1,109 @@
+/*
+                window.group_id,
+                window.participant_id,
+                window.essay_id,
+                field_id,
+                original_value,
+                current_value,
+                correct_answer,
+                (span.className.match(/quota/i))
+*/
+
+
+// Russell's correction function
+window.send_correction = function( group_id, participant_id, essay_id, field_id, original_value, current_value, correct_answer, quota, round_number ) {
+    var params = {
+        "user_id": window.user.id,
+        "participant_id": participant_id,
+        "group": group_id,
+        "response": {
+            essay: essay_id,
+            error: field_id,
+            uncorrected: original_value,
+            corrected: current_value,
+            correct: ( correct_answer == current_value ),
+            correct_answer: correct_answer,
+            quota: quota,
+            round_number: round_number,
+            controller: window.path_to_controller
+        }
+    };
+
+    if ( window.console ) {
+        console.log( "storing data:", params )
+    }
+
+    var str = jQuery.param( params );
+
+    $.ajax( {
+            type: 'POST',
+            url: "responses",
+            data: str,
+            error: function( data ) {
+
+                window.stop_timer();
+                console.log( 'fail', data )
+                $( ".error-modal" ).modal( {
+                    close: false,
+                    overlayId: 'simplemodal-overlay',
+                    containerId: 'simplemodal-container',
+                    onShow: function( dialog ) {
+                      var modal = this;
+                      $(".error-modal").show();
+                      $('.error-modal a.close-modal').click( function(){
+                        modal.close();
+                        window.start_timer();
+                      });
+                    }
+                } );
+            }
+    } );
+}
+
+window.create_user = function(participant_id, group_id) {
+    if ( window.console ) {
+        console.log( "storing user")
+    }
+    $.ajax({
+      type: 'POST',
+      url: "users.json",
+      data: {
+        "participant_id": participant_id,
+        "group": group_id,
+        controller: window.path_to_controller
+      },
+      success: function(data) {
+        window.user = data.user
+      }
+    });
+
+}
+
+window.start_round = function(round_number, user_id){
+    if ( window.console ) {
+        console.log( "updating round")
+    }
+    $.ajax({
+      type: 'POST',
+      url: "rounds.json",
+      data: {
+        "user_id": user_id,
+        "round_number": round_number,
+        controller: window.path_to_controller
+      },
+      success: function(data) {
+        window.round = data.round
+      }
+    });
+}
+
+window.complete_round = function(){
+    $.get(window.path_to_controller + '/score_card', {
+      user_id: window.user.id,
+      round_number: window.round_number,
+      round_time: window.elapsed_time_in_seconds,
+      completed_in_time: window.completed_in_time
+    }, function(data) {
+      $('.body').html(data);
+    });
+}
