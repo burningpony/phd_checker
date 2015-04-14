c46339568559d0cb7e8f9c95f32b640729ea336a
Russell Osborne <russosborn@gmail.com>
2015-04-14 17:07:58 -0400

    Refactoring user model, and showing all raw data when viewing a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,83 +4,51 @@ class User < ActiveRecord::Base
   has_many :rounds, dependent: :destroy
   def self.aggregate_analysis(users)
     CSV.generate do |csv|
-      csv << [
-        'id',
-        'group',
-        'data_id',
-        'treatment',
-        'total_time_taken',
-        'total_edited',
-        'total_correct',
-        'total_earned',
-        'cumulative_impact',
-        'finished_early',
-        'created_at',
-        'round_1_edit',
-        'round_1_correct',
-        'round_2_edit',
-        'round_2_correct',
-        'round_3_edit',
-        'round_3_correct',
-        'round_4_edit',
-        'round_4_correct',
-        'time_to_complete_round_1',
-        'time_to_complete_round_2',
-        'time_to_complete_round_3',
-        'time_to_complete_round_4',
-        'round_1_earned',
-        'round_2_earned',
-        'round_3_earned',
-        'round_4_earned',
-        'counter_part_impact_1',
-        'counter_part_impact_2',
-        'counter_part_impact_3',
-        'counter_part_impact_4',
-        'finish_round_1_early',
-        'finish_round_2_early',
-        'finish_round_3_early',
-        'finish_round_4_early'
-      ]
+      csv << User.first.analyze.keys
       users.sort_by{|u| u.group}.each do |user|
-        csv << [
-          user.id,
-          user.group,
-          user.participant_id,
-          user.rounds.pluck(:name).first.try(:to_i),
-          user.rounds.sum(:time_to_complete_in_seconds),
-          user.responses.count,
-          user.responses.where(correct: true).count,
-          user.total_payment,
-          User.counter_part_impact(user.responses.where(correct: true).count, user.responses.where(correct: false).count),
-          user.rounds.where(completed_in_time: true).present?.to_i,
-          user.created_at,
-          user.responses.where(round_number: 1).count,
-          user.responses.where(round_number: 1, correct: true).count,
-          user.responses.where(round_number: 2).count,
-          user.responses.where(round_number: 2, correct: true).count,
-          user.responses.where(round_number: 3).count,
-          user.responses.where(round_number: 3, correct: true).count,
-          user.responses.where(round_number: 4).count,
-          user.responses.where(round_number: 4, correct: true).count,
-          user.rounds.where(round_number: 1).pluck(:time_to_complete_in_seconds).first,
-          user.rounds.where(round_number: 2).pluck(:time_to_complete_in_seconds).first,
-          user.rounds.where(round_number: 3).pluck(:time_to_complete_in_seconds).first,
-          user.rounds.where(round_number: 4).pluck(:time_to_complete_in_seconds).first,
-          user.rounds.where(round_number: 1).pluck(:round_payment).first,
-          user.rounds.where(round_number: 2).pluck(:round_payment).first,
-          user.rounds.where(round_number: 3).pluck(:round_payment).first,
-          user.rounds.where(round_number: 4).pluck(:round_payment).first,
-          user.round_impact(1),
-          user.round_impact(2),
-          user.round_impact(3),
-          user.round_impact(4),
-          user.rounds.where(round_number: 1).pluck(:completed_in_time).first,
-          user.rounds.where(round_number: 2).pluck(:completed_in_time).first,
-          user.rounds.where(round_number: 3).pluck(:completed_in_time).first,
-          user.rounds.where(round_number: 4).pluck(:completed_in_time).first
-        ]
+        csv << user.analyze.values
       end
     end
+  end
+
+  def analyze
+    {
+      id: id,
+      group: group,
+      data_id: participant_id,
+      treatment: rounds.pluck(:name).first.try(:to_i),
+      total_time_taken: rounds.sum(:time_to_complete_in_seconds),
+      total_edited: responses.count,
+      total_correct: responses.where(correct: true).count,
+      total_earned: total_payment,
+      cumulative_impact: User.counter_part_impact(responses.where(correct: true).count,responses.where(correct: false).count),
+      finished_early: rounds.where(completed_in_time: true).present?.to_i,
+      created_at: created_at,
+      round_1_edit: responses.where(round_number: 1).count,
+      round_1_correct: responses.where(round_number: 1, correct: true).count,
+      round_2_edit: responses.where(round_number: 2).count,
+      round_2_correct: responses.where(round_number: 2, correct: true).count,
+      round_3_edit: responses.where(round_number: 3).count,
+      round_3_correct: responses.where(round_number: 3, correct: true).count,
+      round_4_edit: responses.where(round_number: 4).count,
+      round_4_correct: responses.where(round_number: 4, correct: true).count,
+      time_to_complete_round_1: rounds.where(round_number: 1).pluck(:time_to_complete_in_seconds).first,
+      time_to_complete_round_2: rounds.where(round_number: 2).pluck(:time_to_complete_in_seconds).first,
+      time_to_complete_round_3: rounds.where(round_number: 3).pluck(:time_to_complete_in_seconds).first,
+      time_to_complete_round_4: rounds.where(round_number: 4).pluck(:time_to_complete_in_seconds).first,
+      round_1_earned: rounds.where(round_number: 1).pluck(:round_payment).first,
+      round_2_earned: rounds.where(round_number: 2).pluck(:round_payment).first,
+      round_3_earned: rounds.where(round_number: 3).pluck(:round_payment).first,
+      round_4_earned: rounds.where(round_number: 4).pluck(:round_payment).first,
+      counter_part_impact_1: round_impact(1),
+      counter_part_impact_2: round_impact(2),
+      counter_part_impact_3: round_impact(3),
+      counter_part_impact_4: round_impact(4),
+      finish_round_1_early: rounds.where(round_number: 1).pluck(:completed_in_time).first,
+      finish_round_2_early: rounds.where(round_number: 2).pluck(:completed_in_time).first,
+      finish_round_3_early: rounds.where(round_number: 3).pluck(:completed_in_time).first,
+      finish_round_4_early: rounds.where(round_number: 4).pluck(:completed_in_time).first
+    }
   end
 
   def round_impact(round)
@@ -90,6 +58,4 @@ class User < ActiveRecord::Base
   def self.counter_part_impact(number_correct, number_wrong)
     0.15 * number_correct.to_f - 0.05 * number_wrong.to_f
   end
-
-
 end
