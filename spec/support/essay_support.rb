f2e9e029b81eec964afcd24b36e25e8e83243938
Russell Osborne <russosborn@gmail.com>
2015-04-14 23:40:08 -0400

    Drying up testing, and doing full run through on f and partial on a.

--- spec/support/essay_support.rb
+++ spec/support/essay_support.rb
@@ -1,8 +1,27 @@
-def correct_essay(number)
+def correct_essay(round, number)
+  sleep 0.5
   click_link "Essay #{number}"
-  within "#essay_1_#{number}" do
+  within "#essay_#{round}_#{number}" do
     page.all(:css, '.correctme').each do |el|
       el.set el[:rel]
     end
   end
+end
+
+def complete_round(&block)
+  finish_stage
+  block.call if block_given?
+  click_button 'Done'
+end
+
+def finish_stage
+  click_link 'Finish'
+  click_button 'Confirm'
+end
+
+def snapshot_user_page
+  page.execute_script('window.onbeforeunload = null;')
+  page.execute_script('window.onunload = null;')
+  visit user_path(id: User.last.id)
+  screenshot_and_open_image
 end
