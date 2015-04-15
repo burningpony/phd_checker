91e22982e988a09057f54e76b87ec116894a6af0
scong <sam@burningpony.com>
2015-04-14 20:40:18 -0400

    catch nil

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,6 +2,9 @@ class Round < ActiveRecord::Base
   belongs_to :user
 
   def calculated_time
+    if end_time == nil || created_at == nil
+      return nil
+    end
     ((end_time - created_at) * 24 * 60 * 60).to_i
   end
 end
