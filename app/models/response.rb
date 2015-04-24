2e0569d7d44844fe42c14f0f2946e6614573610f
scong <sam@burningpony.com>
2015-04-24 14:52:17 -0400

    validate length of response

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,7 @@ class Response < ActiveRecord::Base
   require 'csv'
   belongs_to :user
   validates_presence_of :user_id
+  validates :corrected, length: {maximum: 100}
   scope :incorrect, where(correct: false)
   scope :correct, where(correct: true)
 
