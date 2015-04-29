a4e315b7d01168eafb7aabded64cf26b4e86f20b
Russell Osborne <russosborn@gmail.com>
2015-04-29 10:45:40 -0400

    updating routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 PhdChecker::Application.routes.draw do
 
-  match 'a/score_card'                               => 'a#score_card'
-  match 'b/score_card'                               => 'b#score_card'
-  match 'c/score_card'                               => 'c#score_card'
-  match 'd/score_card'                               => 'd#score_card'
-  match 'e/score_card'                               => 'e#score_card'
-  match 'f/score_card'                               => 'f#score_card'
-  match 'g/score_card'                               => 'g#score_card'
-  match 'h/score_card'                               => 'h#score_card'
+  get 'a/score_card'                               => 'a#score_card'
+  get 'b/score_card'                               => 'b#score_card'
+  get 'c/score_card'                               => 'c#score_card'
+  get 'd/score_card'                               => 'd#score_card'
+  get 'e/score_card'                               => 'e#score_card'
+  get 'f/score_card'                               => 'f#score_card'
+  get 'g/score_card'                               => 'g#score_card'
+  get 'h/score_card'                               => 'h#score_card'
 
   resources :a
   resources :b
@@ -18,17 +18,17 @@ PhdChecker::Application.routes.draw do
   resources :g
   resources :h
 
-  match 'responses/empty' => 'responses#empty'
-  match 'responses/export_raw' => 'responses#export_raw_csv'
-  match 'users/export_aggregate_analysis' => 'users#export_aggregate_analysis'
+  post 'responses/empty' => 'responses#empty'
+  get 'responses/export_raw' => 'responses#export_raw_csv'
+  get 'users/export_aggregate_analysis' => 'users#export_aggregate_analysis'
   resources :responses
-  match 'users/complete' => 'users#mark_completed'
-  match 'users/stats' => 'users#stats'
+  post 'users/complete' => 'users#mark_completed'
+  get 'users/stats' => 'users#stats'
   resources :users
   root to: 'users#experiments'
   resources :rounds
 
   # Note: This route will make all actions in
   # every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  get ':controller(/:action(/:id(.:format)))'
 end
