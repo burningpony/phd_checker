d6095ab395c2c1b0e8caaf0024511b70c3356db7
Russell Osborne <russosborn@gmail.com>
2015-04-30 18:29:51 -0400

    have c play all essays. expect $75

--- spec/features/c_games_spec.rb
+++ spec/features/c_games_spec.rb
@@ -13,33 +13,57 @@ RSpec.feature 'CGames', type: :feature, js: true do
     sleep 0.3
     click_link 'Start'
 
-    correct_essay(1, 1)
-    correct_essay(1, 2)
+    (1..10).each do |i|
+      correct_essay(1, i)
+    end
+
     complete_round do
       expect(subject).to have_content('Total earnings so far:')
 
       within '.earnings' do
-        expect(subject).to have_content('$4.80')
+        expect(subject).to have_content('$18.60')
       end
 
       within '.round_earnings' do
-        expect(subject).to have_content('$4.80')
+        expect(subject).to have_content('$18.60')
       end
     end
 
-    correct_essay(2, 3)
-    correct_essay(2, 4)
+    (1..10).each do |i|
+      correct_essay(2, i)
+    end
 
-    complete_round do
-      expect(subject).to have_content('Total earnings so far:')
+    complete_round
 
-      within '.earnings' do
-        expect(subject).to have_content('$9.00')
-      end
+    (1..10).each do |i|
+      correct_essay(3, i)
+    end
 
-      within '.round_earnings' do
-        expect(subject).to have_content('$4.20')
-      end
+    complete_round
+
+    (1..10).each do |i|
+      correct_essay(4, i)
+    end
+
+    finish_stage
+
+    expect(subject).to have_content('Total earnings so far:')
+
+    within '.earnings' do
+      expect(subject).to have_content('$75.00')
+    end
+
+    within '.round_earnings' do
+      expect(subject).to have_content('$18.60')
+    end
+
+    click_link 'Prepare this computer for the next trial'
+
+    within_window open_new_window do
+      page.driver.resize(2000, 900)
+      visit user_path(id: User.last.id)
+      verify_responses
+      expect(find('td.total_earned').text.to_f).to eq(75.0)
     end
 
     expect(User.last.analyze[:finish_round_1_early]).to have_content(1)
