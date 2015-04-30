cdfe1695ca39e3f44b038798833c9b5be65f17ad
Russell Osborne <russosborn@gmail.com>
2015-04-30 11:59:00 -0400

    Test time taken. and time reported.

--- spec/features/f_games_spec.rb
+++ spec/features/f_games_spec.rb
@@ -73,7 +73,7 @@ RSpec.feature 'FGames', type: :feature, js: true do
     click_button 'start'
     sleep 0.3
     click_link 'Start'
-
+    @start_time = Time.now
     (1..10).each do |i|
       correct_essay(1, i, get_it_right: false)
     end
@@ -108,12 +108,23 @@ RSpec.feature 'FGames', type: :feature, js: true do
 
     sleep 0.3
     finish_stage
-
+    @end_time = Time.now
     expect(subject).to have_content('Total earnings so far:')
     expect(User.last.analyze[:finish_round_4_early]).to have_content(1)
     click_link 'Prepare this computer for the next trial'
 
-    # snapshot_user_page
+    within_window open_new_window do
+      page.driver.resize(2000, 900)
+      visit user_path(id: User.last.id)
+      # verify_responses
+      screenshot_and_open_image
+      time_elapsed = (@end_time - @start_time).ceil
+      expect(find('td.total_time_taken').text.to_f).to be_within(5).of(time_elapsed)
+      expect(find('td.calc_time_to_complete_round_1').text.to_f).to be < time_elapsed
+      expect(find('td.calc_time_to_complete_round_2').text.to_f).to be < time_elapsed
+      expect(find('td.calc_time_to_complete_round_3').text.to_f).to be < time_elapsed
+      expect(find('td.calc_time_to_complete_round_4').text.to_f).to be < time_elapsed
+    end
 
     # it gets them all wrong anyway derp derp
 
