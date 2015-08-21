fce40b2bc84da7b551ed87a74ba45b38c50a134f
Russell Osborne <russell@burningpony.com>
2015-08-20 22:54:38 -0400

    Add id's so we can test options and essay selection

--- spec/features/abuse_the_games_spec.rb
+++ spec/features/abuse_the_games_spec.rb
@@ -7,6 +7,49 @@ RSpec.feature 'AbuseGames', type: :feature, js: true do
   end
   scenario 'completes the test with entering no answers' do
     visit root_path
+    within '#option-1' do
+      click_button 'Select'
+    end
+    within '#experiment-a' do
+      click_button 'Begin'
+    end
+    fill_in :participant_id, with: 100
+    fill_in :group_id, with: 400
+    click_button 'start'
+    sleep 0.3
+    click_link 'Start'
+
+    complete_round
+    sleep 0.3
+    complete_round
+    sleep 0.3
+    complete_round do
+      expect(subject).to have_content('Total earnings so far:')
+
+      within '.earnings' do
+        expect(subject).to have_content('$0.00')
+      end
+
+      within '.round_earnings' do
+        expect(subject).to have_content('$0.00')
+      end
+    end
+
+    finish_stage
+
+    expect(subject).to have_content('Total earnings so far:')
+    expect(User.last.analyze[:finish_round_4_early]).to have_content(1)
+    expect(User.last.responses.count).to eq(0)
+    click_link 'Prepare this computer for the next trial'
+
+    snapshot_user_page
+  end
+
+  scenario 'completes the test with entering no answers' do
+    visit root_path
+    within '#option-2' do
+      click_button 'Select'
+    end
     within '#experiment-a' do
       click_link 'Begin'
     end
@@ -44,43 +87,9 @@ RSpec.feature 'AbuseGames', type: :feature, js: true do
 
   scenario 'completes the test with entering no answers' do
     visit root_path
-    within '#experiment-a' do
-      click_link 'Begin'
+    within '#option-3' do
+      click_button 'Select'
     end
-    fill_in :participant_id, with: 100
-    fill_in :group_id, with: 400
-    click_button 'start'
-    sleep 0.3
-    click_link 'Start'
-
-    complete_round
-    sleep 0.3
-    complete_round
-    sleep 0.3
-    complete_round do
-      expect(subject).to have_content('Total earnings so far:')
-
-      within '.earnings' do
-        expect(subject).to have_content('$0.00')
-      end
-
-      within '.round_earnings' do
-        expect(subject).to have_content('$0.00')
-      end
-    end
-
-    finish_stage
-
-    expect(subject).to have_content('Total earnings so far:')
-    expect(User.last.analyze[:finish_round_4_early]).to have_content(1)
-    expect(User.last.responses.count).to eq(0)
-    click_link 'Prepare this computer for the next trial'
-
-    snapshot_user_page
-  end
-
-  scenario 'completes the test with entering no answers' do
-    visit root_path
     within '#experiment-a' do
       click_link 'Begin'
     end
@@ -118,6 +127,9 @@ RSpec.feature 'AbuseGames', type: :feature, js: true do
 
   scenario 'abuse the essay links' do
     visit root_path
+    within '#option-2' do
+      click_button 'Select'
+    end
     within '#experiment-f' do
       click_link 'Begin'
     end
@@ -137,6 +149,5 @@ RSpec.feature 'AbuseGames', type: :feature, js: true do
       click_link "Essay #{i}"
       expect(page).to_not have_content('<input type="text" ')
     end
-
   end
 end
