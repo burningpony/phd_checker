3ae678b56a0cf4948ae4e6616082dcf2e6fac1bb
Patrick Nielsen <ptnielsen55@gmail.com>
2017-08-08 19:09:50 -0400

    Update routes moving application to run under /phase_two

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,49 @@
 PhdChecker::Application.routes.draw do
-  get 'a/score_card'                               => 'a#score_card'
-  get 'b/score_card'                               => 'b#score_card'
-  get 'c/score_card'                               => 'c#score_card'
-  get 'd/score_card'                               => 'd#score_card'
-  get 'e/score_card'                               => 'e#score_card'
-  get 'f/score_card'                               => 'f#score_card'
-  get 'g/score_card'                               => 'g#score_card'
-  get 'h/score_card'                               => 'h#score_card'
+  get ':phase/a/score_card'                               => 'a#score_card'
+  get ':phase/b/score_card'                               => 'b#score_card'
+  get ':phase/c/score_card'                               => 'c#score_card'
+  get ':phase/d/score_card'                               => 'd#score_card'
+  get ':phase/e/score_card'                               => 'e#score_card'
+  get ':phase/f/score_card'                               => 'f#score_card'
+  get ':phase/g/score_card'                               => 'g#score_card'
+  get ':phase/h/score_card'                               => 'h#score_card'
 
-  resources :a
-  resources :b
-  resources :c
-  resources :d
-  resources :e
-  resources :f
-  resources :g
-  resources :h
+  namespace :phase_two do
+    resources :a
+    resources :b
+    resources :c
+    resources :d
+    resources :e
+    resources :f
+    resources :g
+    resources :h
+    resources :rounds
+    resources :responses
+  end
 
   post 'responses/empty' => 'responses#empty'
   get 'responses/export_raw' => 'responses#export_raw_csv'
   get 'users/export_aggregate_analysis' => 'users#export_aggregate_analysis'
   resources :responses
 
-  post 'users/complete' => 'users#mark_completed'
+  post ':phase/users/complete' => 'users#mark_completed'
   get 'users/stats' => 'users#stats'
 
+  # Might need this in order to properly save users
+  # since ajax call includes phase in request
+  post ':phase/users(.:format)' => 'users#create'
+
   resources :users
-  resources :rounds
+  # resources :rounds
 
   get 'setup/payment' => 'setup#payment'
   get 'setup/available_payments' => 'setup#available_payments'
   get 'setup/experiment_options' => 'setup#experiment_options'
-  root to: 'setup#experiment_options'
+
+  # phase_two is the default option so we will redirect
+  # there from the root.  
+  root to: redirect('/phase_two')
+  get 'phase_two' => 'phase_two/setup#experiment_options'
 
   # Note: This route will make all actions in
   # every controller accessible via GET requests.
