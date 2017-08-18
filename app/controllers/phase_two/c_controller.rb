f4087cf29371aa66af3a1f02a3a16da6670e9d72
Patrick Nielsen <ptnielsen55@gmail.com>
2017-08-17 22:49:41 -0400

    Remove duplicated controllers

--- app/controllers/phase_two/c_controller.rb
+++
@@ -1,9 +0,0 @@
-class PhaseTwo::CController <  EssayBaseController
-  def score_card
-    payment = Payment.new(@option).c(total_responses: @total_responses.count, responses_from_round: @responses_from_round.count)
-    @total_payment =  payment.total_payment
-    @round_payment =  payment.round_payment
-    @name = '2'
-    render file: 'essays/score_card', formats: [:html], layout: false
-  end
-end
